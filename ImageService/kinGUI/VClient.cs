378367dc743acda3257f18d3865d0130df8d7d7b
Itay <itayh.msn@hotmail.com>
2018-04-30 23:14:23 +0300

    Add gui and start writing client

---
+++ ImageService/kinGUI/VClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace kinGUI
+{
+    class VClient
+    {
+        private Socket sender;
+
+        public VClient(string ip, int port)
+        {
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+                this.sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+            } catch (Exception e)
+            {
+
+            }
+    }
+}
